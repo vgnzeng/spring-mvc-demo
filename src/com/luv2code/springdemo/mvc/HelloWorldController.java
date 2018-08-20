b1f3165025930d14c0640c5f72738a6f243d8f81
Guannian Zeng <vgnzeng@gmail.com>
2018-08-20 13:16:23 -0700

    added @RequestParam to bind input Form data

--- src/com/luv2code/springdemo/mvc/HelloWorldController.java
+++ src/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -22,7 +23,7 @@ public class HelloWorldController {
 	public String processForm() {
 		return "helloworld";
 	}
-
+	
 	// new a controller method to read form data and
 	// add data to the model
 
@@ -43,6 +44,25 @@ public class HelloWorldController {
 				
 		return "helloworld";
 	}
+	
+	@RequestMapping("/processFormVersionThree")	
+	public String processFormVersionThree(
+			@RequestParam("studentName") String theName, 
+			Model model) {
+				
+		// convert the data to all caps
+		theName = theName.toUpperCase();
+		
+		// create the message
+		String result = "Hey My Friend from v3! " + theName;
+		
+		// add message to the model
+		model.addAttribute("message", result);
+				
+		return "helloworld";
+	}	
+	
+	
 	
 }
 
