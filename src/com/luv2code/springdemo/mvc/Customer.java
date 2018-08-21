33e9b0f66215c38a82b9e8bc397a12fcdc7a52e6
Guannian Zeng <vgnzeng@gmail.com>
2018-08-20 20:32:02 -0700

    added new field to Customer Model with Regular Expression support

--- src/com/luv2code/springdemo/mvc/Customer.java
+++ src/com/luv2code/springdemo/mvc/Customer.java
@@ -3,6 +3,7 @@ package com.luv2code.springdemo.mvc;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 public class Customer {
@@ -17,6 +18,17 @@ public class Customer {
 	@Max(value=10, message="must be less than or equal to 10")
 	private int freePasses;
 
+	@Pattern(regexp="^[a-zA-Z0-9]{5}", message="only 5 chars/digits")
+	private String postalCode;
+	
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+	
 	public int getFreePasses() {
 		return freePasses;
 	}
